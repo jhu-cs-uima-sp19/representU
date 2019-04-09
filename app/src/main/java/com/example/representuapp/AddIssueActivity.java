a4b1ab0c66e7c87a81f2e2aa7671537cf5e74cb8
Aubin C Lohier <aublhr99@gmail.com>
2019-04-09 17:38:59 -0400

    Revert "WIP"
    This reverts commit 48bb4b5a99254fdb123cda68bd7bf3f17d8f5ee2.

--- app/src/main/java/com/example/representuapp/AddIssueActivity.java
+++ app/src/main/java/com/example/representuapp/AddIssueActivity.java
@@ -4,12 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class AddIssueActivity extends AppCompatActivity {
 
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +13,10 @@ public class AddIssueActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_issue);
 
         Intent intent = getIntent();
-        //DatabaseReference myRef = database.getReference("message");
-        //myRef.setValue("Hello, World!");
-
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        saveIssue();
 
 
-    }
-
-    public void saveIssue() {
 
     }
 }
