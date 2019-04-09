b674c570f7202be000e14b90165f0516eb77e03f
miaboloix <MbMv2017>
2019-04-09 17:44:34 -0400

    small tweeks

--- app/src/main/java/com/example/representuapp/UserFeedActivity.java
+++ app/src/main/java/com/example/representuapp/UserFeedActivity.java
@@ -23,15 +23,6 @@ public class UserFeedActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -80,9 +71,7 @@ public class UserFeedActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.userFeed) {
-            // Handle the camera action
-        } else if (id == R.id.meet_your_sga) {
+        if (id == R.id.meet_your_sga) {
 
         } else if (id == R.id.settingsUser) {
 
