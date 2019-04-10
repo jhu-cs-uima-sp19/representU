33ec151458d03e8ccf7f2f97773205f6c028b383
Aubin C Lohier <aublhr99@gmail.com>
2019-04-10 00:12:00 -0400

    Add save icon, wire button to save issue from test fields

--- app/src/main/java/com/example/representuapp/AddIssueActivity.java
+++ app/src/main/java/com/example/representuapp/AddIssueActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -47,7 +50,15 @@ public class AddIssueActivity extends AppCompatActivity {
     }
 
     public void saveIssue() {
-        Issue issue = new Issue("Test Title", "Summary Test");
-        dbRef.child("issues").child(issue.idNum.toString()).child(issue.title).setValue(issue);
+        final EditText issueTitle = findViewById(R.id.issue_title);
+        final EditText issueSum = findViewById(R.id.issue_summary);
+        Button saveButton = findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Issue issue = new Issue(issueTitle.getText().toString(), issueSum.getText().toString());
+                dbRef.child("issues").child(issue.idNum.toString()).child(issue.title).setValue(issue);
+            }
+        });
     }
 }
