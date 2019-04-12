7761ea3f9a6b15bd0730f1f2664a5ff6a58ca14f
miaboloix <MbMv2017>
2019-04-12 15:40:56 -0400

    added save icon to Edit Issue activity bar

--- app/src/main/java/com/example/representuapp/AddIssueActivity.java
+++ app/src/main/java/com/example/representuapp/AddIssueActivity.java
@@ -27,27 +27,6 @@ public class AddIssueActivity extends AppCompatActivity {
         saveIssue();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.edit_issue, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_save) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void saveIssue() {
         final EditText issueTitle = findViewById(R.id.issue_title);
--- app/src/main/java/com/example/representuapp/EditIssueActivity.java
+++ app/src/main/java/com/example/representuapp/EditIssueActivity.java
@@ -3,6 +3,8 @@ package com.example.representuapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.google.firebase.database.ChildEventListener;
@@ -96,5 +98,28 @@ public class EditIssueActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError dataError) {
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.edit_issue, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_save) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
