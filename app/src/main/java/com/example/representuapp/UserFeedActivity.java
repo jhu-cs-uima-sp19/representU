ee745123f58f0821679a81c53ff64d7fe7c85518
miaboloix <MbMv2017>
2019-04-20 17:59:23 -0700

    added confirmation alert for deleting an issue

--- app/src/main/java/com/example/representuapp/UserFeedActivity.java
+++ app/src/main/java/com/example/representuapp/UserFeedActivity.java
@@ -202,27 +202,25 @@ public class UserFeedActivity extends AppCompatActivity
 
         } else if (id == R.id.logout) {
             //set message
-            alertDialogBuilder.setTitle(R.string.logout_confirm);
-            alertDialogBuilder.setCancelable(false);
+            alertDialogBuilder.setTitle(R.string.logout_confirm).setCancelable(false);
 
-
+            //define yes button
             alertDialogBuilder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
-                    Toast.makeText(getApplicationContext(), "Logging You Out", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Logged Out", Toast.LENGTH_SHORT).show();
                     finish();
                 }
             });
+
+            //define cancel button
             alertDialogBuilder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
-                    Toast.makeText(getApplicationContext(), "You Clicked CANCEL", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_SHORT).show();
                 }
             });
 
-            // create alert dialog
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            // show it
-            alertDialog.show();
-
+            // create and show alert dialog
+            alertDialogBuilder.create().show();
 
         } else if (id == R.id.changePassword) {
 
