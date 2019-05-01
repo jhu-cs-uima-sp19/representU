1bb051582e9074694e9112c9bbb5ef18e45af3f7
Peyton Baur <pbaur1@jhu.edu>
2019-05-01 12:12:38 -0400

    starting user side of sga

--- app/src/main/java/com/example/representuapp/SGAMemberActivity.java
+++ app/src/main/java/com/example/representuapp/SGAMemberActivity.java
@@ -13,17 +13,8 @@ public class SGAMemberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sgamember);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
