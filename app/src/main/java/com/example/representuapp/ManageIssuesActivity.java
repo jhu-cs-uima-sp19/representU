78fe91f8e47de212c7c47df6bf827d7272a59160
Peyton Baur <pbaur1@jhu.edu>
2019-05-01 22:13:35 -0400

    put button in action bar

--- app/src/main/java/com/example/representuapp/ManageIssuesActivity.java
+++ app/src/main/java/com/example/representuapp/ManageIssuesActivity.java
@@ -3,6 +3,8 @@ package com.example.representuapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -45,15 +47,6 @@ public class ManageIssuesActivity extends AppCompatActivity {
         Intent intent = getIntent();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Add button
-        add = (Button) findViewById(R.id.addIssue);
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ManageIssuesActivity.this, AddIssueActivity.class);
-                startActivity(intent);
-            }
-        });
 
         issuesList = (ListView) findViewById(R.id.issuesList);
 
@@ -72,7 +65,6 @@ public class ManageIssuesActivity extends AppCompatActivity {
                     arrayList.add(string);
                     idList.add(id);
                     adapter.notifyDataSetChanged();
-
                 }
 
             }
@@ -93,5 +85,29 @@ public class ManageIssuesActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.edit_sga, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+            Intent intent = new Intent(ManageIssuesActivity.this, AddIssueActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
